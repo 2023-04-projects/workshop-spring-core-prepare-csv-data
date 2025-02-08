26a0364989444f272f40e34699ba7d4048aa2f28
khadri2021 <khadri.institutions@gmail.com>
2025-02-08 12:16:20 +0530

    initial changes

---
+++ src/main/java/com/khadri/spring/core/csv/prepare/data/config/AppConfig.java
@@ -0,0 +1,8 @@
+package com.khadri.spring.core.csv.prepare.data.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+}
---
+++ src/main/java/com/khadri/spring/core/csv/prepare/data/driver/CsvDataDriver.java
@@ -0,0 +1,10 @@
+package com.khadri.spring.core.csv.prepare.data.driver;
+
+public class CsvDataDriver {
+
+	public static void main(String[] args) {
+
+		// Activation of Container : BeanFactory or ApplicationContext Container
+
+	}
+}
