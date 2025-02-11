57073bc96f9e28788fea218e6d67551ae006db26
sgreddy123 <151907854+sgreddy123@users.noreply.github.com>
2025-02-11 12:10:30 +0530

    Khadri 09022025 1243 pm (#1)
    * customer module
    
    * customer
    
    * formated files
    
    * changed appConfig
    
    * removed unwanted sysout
    
    * removed unwanted sysouts
    
    * updated code

--- src/main/java/com/khadri/spring/core/csv/prepare/data/config/AppConfig.java
+++ src/main/java/com/khadri/spring/core/csv/prepare/data/config/AppConfig.java
@@ -1,8 +1,37 @@
 package com.khadri.spring.core.csv.prepare.data.config;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import com.khadri.spring.core.csv.prepare.data.customer.processor.CustomerDataProcessor;
 
 @Configuration
 public class AppConfig {
+
+	@Bean
+	public Scanner scanner() {
+		return new Scanner(System.in);
+	}
+
+	@Bean
+	public File customerFile() {
+		return new File("src/main/resources/customer.csv");
+	}
+
+	@Bean
+	public PrintWriter printWriter(File file) throws Exception {
+		FileWriter fileWriter = new FileWriter(file, true);
+		return new PrintWriter(fileWriter);
+	}
+
+	@Bean
+	public CustomerDataProcessor customerDataProcessor() {
+		return new CustomerDataProcessor(scanner());
+	}
 
 }
--- src/main/java/com/khadri/spring/core/csv/prepare/data/driver/CsvDataDriver.java
+++ src/main/java/com/khadri/spring/core/csv/prepare/data/driver/CsvDataDriver.java
@@ -1,10 +1,81 @@
 package com.khadri.spring.core.csv.prepare.data.driver;
 
+import java.io.File;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.Scanner;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.khadri.spring.core.csv.prepare.data.config.AppConfig;
+import com.khadri.spring.core.csv.prepare.data.customer.processor.CustomerDataProcessor;
+import com.khadri.spring.core.csv.prepare.data.customer.util.CustomerFileUtil;
+import com.khadri.spring.core.csv.prepare.data.drivertypes.DriverTypes;
+
+import io.vavr.Tuple2;
+
 public class CsvDataDriver {
 
-	public static void main(String[] args) {
+	private static CustomerDataProcessor customerDataProcessor;
+	private static Scanner scanner;
+	private static CsvDataDriver csvDriver;
+	private static ApplicationContext applicationContext;
 
-		// Activation of Container : BeanFactory or ApplicationContext Container
+	{
+		applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+		scanner = applicationContext.getBean(Scanner.class);
+
+		customerDataProcessor = applicationContext.getBean(CustomerDataProcessor.class);
 
 	}
+
+	public static void main(String[] args) {
+		csvDriver = new CsvDataDriver();
+
+		System.out.println("###################### Welocme to CSV data Driver  ########################");
+		System.out.println("$$$$$$$$$$$$$$ The data procesors $$$$$$$$$$$$$$$$$");
+
+		Arrays.stream(DriverTypes.values()).forEach(each -> {
+			System.out.println(each.getId() + " : " + each.getName());
+		});
+
+		csvDriver.process();
+
+	}
+
+	private void process() {
+		System.out.println("Please choose the data processor : ");
+		int dataProcessId = scanner.nextInt();
+
+		switch (dataProcessId) {
+
+		case 3:
+
+			Tuple2<File, PrintWriter> tupleFilePW2 = CustomerFileUtil.customerCsvFilePrintWriter(applicationContext);
+
+			try {
+				System.out.println(" How many records you want to enter ?  : ");
+				int records2 = scanner.nextInt();
+
+				for (int i = 1; i <= records2; i++) {
+					customerDataProcessor.process(tupleFilePW2._2, i);
+				}
+			} catch (Exception e) {
+				System.out.println("Exception Occured " + e.getCause());
+			} finally {
+				System.out.println("The remaining records were inserted into file. ");
+				tupleFilePW2._2.flush();
+				tupleFilePW2._2.close();
+			}
+			break;
+
+		default:
+			break;
+		}
+
+		((AnnotationConfigApplicationContext) applicationContext).close();
+	}
+
 }
