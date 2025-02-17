0b5145a705e6887ee89aeb0e05e37a39a2666e10
manvithagali52 <manvithag52@gmail.com>
2025-02-17 12:33:31 +0530

    employee module (#2)
    * employee module
    
    * updated code
    
    * i removed unwanted code
    
    * removed unwanted code
    
    * updated appconfig class

--- src/main/java/com/khadri/spring/core/csv/prepare/data/config/AppConfig.java
+++ src/main/java/com/khadri/spring/core/csv/prepare/data/config/AppConfig.java
@@ -1,31 +1,38 @@
 package com.khadri.spring.core.csv.prepare.data.config;
 
+import java.beans.BeanProperty;
 import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Scanner;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import com.khadri.spring.core.csv.prepare.data.customer.processor.CustomerDataProcessor;
+import com.khadri.spring.core.csv.prepare.data.employee.processor.EmployeeDataProcessor;
 
 @Configuration
 public class AppConfig {
-
 	@Bean
 	public Scanner scanner() {
 		return new Scanner(System.in);
 	}
-
-	@Bean
-	public File customerFile() {
-		return new File("src/main/resources/customer.csv");
+     @Bean("employeeFile")
+	public File employeeFile() {
+    	 String path="src/main/resources/employee.csv";
+		return new File(path);
 	}
 
 	@Bean
-	public PrintWriter printWriter(File file) throws Exception {
-		FileWriter fileWriter = new FileWriter(file, true);
+	public EmployeeDataProcessor employeeDataProcessor() {
+		return new EmployeeDataProcessor(scanner());
+	}
+
+	@Bean
+	public PrintWriter printWriterEmployee(@Qualifier("employeeFile")File employeeFile) throws IOException {
+		FileWriter fileWriter = new FileWriter(employeeFile,true);
 		return new PrintWriter(fileWriter);
 	}
 
--- src/main/java/com/khadri/spring/core/csv/prepare/data/driver/CsvDataDriver.java
+++ src/main/java/com/khadri/spring/core/csv/prepare/data/driver/CsvDataDriver.java
@@ -4,11 +4,11 @@ import java.io.File;
 import java.io.PrintWriter;
 import java.util.Arrays;
 import java.util.Scanner;
-
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
 import com.khadri.spring.core.csv.prepare.data.config.AppConfig;
+import com.khadri.spring.core.csv.prepare.data.employee.processor.EmployeeDataProcessor;
+import com.khadri.spring.core.csv.prepare.data.employee.util.EmployeeFileUtil;
 import com.khadri.spring.core.csv.prepare.data.customer.processor.CustomerDataProcessor;
 import com.khadri.spring.core.csv.prepare.data.customer.util.CustomerFileUtil;
 import com.khadri.spring.core.csv.prepare.data.drivertypes.DriverTypes;
@@ -16,19 +16,17 @@ import com.khadri.spring.core.csv.prepare.data.drivertypes.DriverTypes;
 import io.vavr.Tuple2;
 
 public class CsvDataDriver {
-
 	private static CustomerDataProcessor customerDataProcessor;
+	private static EmployeeDataProcessor employeeDataProcessor;
 	private static Scanner scanner;
 	private static CsvDataDriver csvDriver;
 	private static ApplicationContext applicationContext;
 
 	{
 		applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
-
 		scanner = applicationContext.getBean(Scanner.class);
-
+		employeeDataProcessor = applicationContext.getBean(EmployeeDataProcessor.class);
 		customerDataProcessor = applicationContext.getBean(CustomerDataProcessor.class);
-
 	}
 
 	public static void main(String[] args) {
@@ -40,9 +38,7 @@ public class CsvDataDriver {
 		Arrays.stream(DriverTypes.values()).forEach(each -> {
 			System.out.println(each.getId() + " : " + each.getName());
 		});
-
 		csvDriver.process();
-
 	}
 
 	private void process() {
@@ -50,6 +46,22 @@ public class CsvDataDriver {
 		int dataProcessId = scanner.nextInt();
 
 		switch (dataProcessId) {
+		case 2:
+			Tuple2<File, PrintWriter> tupleFilePW1 = EmployeeFileUtil.employeeCsvFilePrintWriter(applicationContext);
+			try {
+				System.out.println(" How many records you want to enter ?  : ");
+				int records1 = scanner.nextInt();
+				for (int i = 1; i <= records1; i++) {
+					employeeDataProcessor.process(tupleFilePW1._2, i);
+				}
+			} catch (Exception e) {
+				System.out.println("Exception Occured" + e.getCause());
+			} finally {
+				System.out.println("The remaining records were inserted into file. ");
+				tupleFilePW1._2.flush();
+				tupleFilePW1._2.close();
+			}
+			break;
 
 		case 3:
 
